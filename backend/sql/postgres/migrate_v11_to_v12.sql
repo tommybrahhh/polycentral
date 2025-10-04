8544ed35da90b6853c5268387ae37d545b4a10f0
roman <romanomaldito@gmail.com>
2025-10-04 09:35:04 +0200

    fix: make constraint rename safe in migrate_v11_to_v12.sql

--- backend/sql/postgres/migrate_v11_to_v12.sql
+++ backend/sql/postgres/migrate_v11_to_v12.sql
@@ -6,8 +6,17 @@ ALTER TABLE IF EXISTS tournaments RENAME TO events;
 ALTER TABLE IF EXISTS tournament_participants RENAME TO event_participants;
 ALTER TABLE IF EXISTS tournament_entries RENAME TO event_entries;
 
--- Rename constraints
-ALTER TABLE IF EXISTS event_participants RENAME CONSTRAINT fk_tournament_id TO fk_event_id;
+-- Rename constraints only if they exist
+DO $$
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM information_schema.table_constraints
+        WHERE table_name = 'event_participants'
+        AND constraint_name = 'fk_tournament_id'
+    ) THEN
+        ALTER TABLE event_participants RENAME CONSTRAINT fk_tournament_id TO fk_event_id;
+    END IF;
+END $$;
 
 -- Rename indexes
 ALTER INDEX IF EXISTS idx_tournament_entries_user RENAME TO idx_event_entries_user;
