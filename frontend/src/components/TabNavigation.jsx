c66c7d9f054d8c551de710ac5ea8462f66fe30a6
roman <romanomaldito@gmail.com>
2025-10-12 12:29:41 +0200

    feat(frontend): add tab navigation component

---
+++ frontend/src/components/TabNavigation.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/TabNavigation.css';
+
+const TabNavigation = ({ tabs, activeTab, onTabChange }) => {
+  return (
+    <div className="tab-navigation">
+      {tabs.map((tab) => (
+        <button
+          key={tab.id}
+          className={`tab-button ${activeTab === tab.id ? 'active' : ''}`}
+          onClick={() => onTabChange(tab.id)}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+TabNavigation.propTypes = {
+  tabs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired
+    })
+  ).isRequired,
+  activeTab: PropTypes.string.isRequired,
+  onTabChange: PropTypes.func.isRequired
+};
+
+export default TabNavigation;
