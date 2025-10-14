e0e610cbd0fbfc016971ed58f924794d234a12de
roman <romanomaldito@gmail.com>
2025-10-14 17:55:04 +0200

    profile page fix

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import TabNavigation from '../components/TabNavigation';
 import ProfileHistory from '../components/ProfileHistory';
+import AdminControlPanel from '../components/AdminControlPanel';
 import useFetch from '../hooks/useFetch';
 import './ProfilePage.css';
 
